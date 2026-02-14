18719f341b78f794375ad9e201093143ff745d12
BuscaCliente <pedrosportes@gmail.com>
2026-02-14 20:11:03 -0300

    menos erros

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,18 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       }
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          }
+        }
+      }
     }
   };
 });
